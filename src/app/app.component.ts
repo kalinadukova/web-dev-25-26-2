6434aa4827417eb100e8d657bc551ef3b370f9cf
Kalina Dukova <kalina.dukova98@gmail.com>
2025-10-28 16:59:14 +0200

    lab 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { MessageModule } from 'primeng/message';
 import { CommonModule } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { Select } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +21,7 @@ import { Select } from 'primeng/select';
     ReactiveFormsModule,
     TableModule,
     Select,
+    FormsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -37,6 +39,7 @@ export class AppComponent {
     { label: 'Princeton University', value: 'Princeton University' }
   ];
 
+
   constructor(private fb: FormBuilder) {
     this.registrationForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
@@ -47,6 +50,22 @@ export class AppComponent {
   }
 
   onSubmit() {
+    let email = this.registrationForm.value.email;
+    let userEmails = this.users.map(user => user.email)
+    if (email) {
+      let domain = email.split('@')[1];
+      if (domain !== 'mail.com') {
+        this.registrationForm.get('email')?.setErrors({invalidDomain: true})
+      } else {
+        this.registrationForm.get('email')?.setErrors(null); 
+        if (userEmails.includes(email)) {
+          this.registrationForm.get('email')?.setErrors({duplicateEmail: true})
+         } else {
+          this.registrationForm.get('email')?.setErrors(null)
+         }
+      }
+   }
+
     if (this.registrationForm.valid) {
       this.users.push(this.registrationForm.value);
       console.log('User added:', this.registrationForm.value);
@@ -57,9 +76,15 @@ export class AppComponent {
     }
   }
 
+
+  onDelete(index: number) {
+    console.log(this.users[index].email)
+    this.users = this.users.splice(0, index)
+  }
+
   getFieldError(fieldName: string): string {
     const field = this.registrationForm.get(fieldName);
-    console.log(field?.errors)
+    // console.log(field?.errors)
     if (field?.touched && field?.invalid) {
       if (field.errors?.['required']) {
         return 'This field is required';
@@ -69,6 +94,12 @@ export class AppComponent {
       }
       if (field.errors?.['email']) {
         return 'Please enter a valid email';
+      }
+      if (field.errors?.['invalidDomain']) {
+        return 'Email domain must be mail.com';
+      }
+      if (field.errors?.['duplicateEmail']) {
+        return 'Email already exists'
       }
     }
     return '';
